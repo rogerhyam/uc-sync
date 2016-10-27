7c32c39e514151e8f8a5beaff682bfd90b86145f
Roger Hyam <roger@hyam.net>
2016-10-27 14:11:16 +0000

    include right file

--- modx.php
+++ modx.php
@@ -1,6 +1,6 @@
 <?php
 
-$url = 'https://uc-sync-rogerhyam.c9users.io/index.php?' . str_replace('&amp;', '&', base64_decode($_GET['qs64']));
+$url = "http".(!empty($_SERVER['HTTPS'])?"s":"") . "://".$_SERVER['SERVER_NAME'] . '/index.php?' . str_replace('&amp;', '&', base64_decode($_GET['qs64']));
 //$url = 'https://uc-sync-rogerhyam.c9users.io/index.php?q=calendar-test&flags=&month=5&garden_id=all';
 readfile($url);
 ?>
